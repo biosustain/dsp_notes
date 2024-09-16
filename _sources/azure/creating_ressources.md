92b5415a934ea694d40b6ad2e2768add0274496b
enryH <enryH@users.noreply.github.com>
2024-09-16 15:09:11 +0000

    deploy: a831522bde32096fd94a443fc887976abc9148fb

---
+++ _sources/azure/creating_ressources.md
@@ -0,0 +1,5 @@
+# Create Azure resources
+
+How to create azure resources in a common VNET
+
+> good first issue for Albert:)
---
+++ _sources/python/percent_notebooks.py
@@ -0,0 +1,23 @@
+# %% [markdown]
+# # Percent Notebooks in VSCode (and other editior)
+# This site is written as a jupyter notebook which is then executed and converted to html
+# by mystnb and sphinx.
+#
+# A brief example:
+# ```
+# # %% [markdown]
+# # # Heading 1
+# # Some text
+# #
+# # %%
+# # print("Hello World")
+#
+# ```
+# Find more information on
+# [jupytext's documentation](https://jupytext.readthedocs.io/en/latest/formats-scripts.html)
+
+# %%
+print("Hello World form a percent notebook")
+
+# %% [markdown]
+# This notebook was executed using `jupytext` and `mystnb` by `sphinx`.
